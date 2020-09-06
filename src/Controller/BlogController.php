7618f97f9fb57859caaaeda92aeaaf845ddf8338
Camilo Parra <caparra92@gmail.com>
2020-09-06 10:15:05 -0500

    Adding methods for index and register pages

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,15 +11,27 @@ class BlogController extends AbstractController
         return $this->render('index.html.twig');
     }
 
-    public function login() {
-        return $this->render('blog/login.html.twig');
-    }
-
     public function register() {
         return $this->render('blog/register.html.twig');
     }
 
     public function home() {
         return $this->render('blog/home.html.twig');
+    }
+
+    public function  contact() {
+        return $this->render('blog/contact.html.twig');
+    }
+
+    public function store() {
+        
+    }
+
+    public function update() {
+        
+    }
+
+    public function destroy() {
+        
     }
 }
